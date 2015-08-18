e98b0094c4c7faa552720ef348511becdc3b5ab0
Darshak Mehta <tech-boy@users.noreply.github.com>
2015-08-18 09:18:12 +0530

    Update baby_steps.js

--- baby_steps.js
+++ baby_steps.js
@@ -1,6 +1,6 @@
-console.log(process.argv); // To display the Command Line Arguments
+//console.log(process.argv); // To display the Command Line Arguments
 var sum = 0;
-for(i = 2 ; i < process.argv.length ; i++){
+for(i = 2 ; i < process.argv.length ; i++){ // i =2 since first is node and second element is path
 	sum+= Number(process.argv [i]);
 }
 

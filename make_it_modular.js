be6a1e2d3ea9d5cea1b55e9aadc170add69a552e
Darshak Mehta <tech-boy@users.noreply.github.com>
2015-08-23 11:04:06 +0530

    Make It modular

---
+++ make_it_modular.js
@@ -0,0 +1,32 @@
+//mymodule.js
+
+var fs = require('fs')
+var path = require('path')
+
+module.exports = function (dir, filterStr, callback) {
+
+fs.readdir(dir, function (err, list) {
+if (err)
+return callback(err)
+
+list = list.filter(function (file) {
+return path.extname(file) === '.' + filterStr
+})
+
+callback(null, list)
+})
+}
+
+//program.js
+var filterFn = require('./mymodule.js')
+var dir = process.argv[2]
+var filterStr = process.argv[3]
+
+filterFn(dir, filterStr, function (err, list) {
+if (err)
+return console.error('There was an error:', err)
+
+list.forEach(function (file) {
+console.log(file)
+})
+})

22e3b508b4cd5d9c88e69ecd653ecf709905e26f
Darshak Mehta <darshak_mehta@live.com>
2015-10-02 11:08:38 +0530

    HTTP get updated

--- main.js
+++ main.js
@@ -1,12 +1,12 @@
-var http = require('http')
+var http = require('http');
 var url = process.argv[2];
 http.get(url,function(response){
-response.setEncoding('utf8') 
+response.setEncoding('utf8'); 
 response.on("data",function(data){
 
 	console.log(data);
 response.on("error",function(error){
-	console.log("error")
-})
-})
-})
+	console.log("error");
+});
+});
+});

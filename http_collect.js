23275cc5e6a0b03c989af38100a02b0cb9711ae0
Darshak Mehta <darshak_mehta@live.com>
2015-10-02 12:25:55 +0530

    HTTP collect using function length

--- http_collect.js
+++ http_collect.js
@@ -1,11 +1,13 @@
 var http=require('http')
 var bl =require('bl');
 http.get(process.argv[2],function(response){
-var count=0
+//var count=0
 response.pipe(bl(function(err,data){
-	for(i=0;i<data.length;i++)
+/*	for(i=0;i<data.length;i++)
 		count++;
 	console.log(count)
+	*/
+	console.log(data.length)
 	console.log(data.toString());
 
 }))
